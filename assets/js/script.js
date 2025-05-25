c8d430210e9fc7b32a30e2a79d30a9bef011f467
EnzoOkuizumiFiap <enzookuizumifiap@gmail.com>
2025-05-25 17:05:19 -0300

    Renomeação do botão de cura para "Curar" e adição do script.js com elementos DOM.

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+    //Elementos DOM
+    const mainGorillaImg = document.getElementById('main-gorilla-img');
+
+    const gorillaHealthProgress = document.getElementById('gorilla-health-progress');
+    const gorillaHealthText = document.getElementById('gorilla-health-text');
+    const gorillaEnergyProgress = document.getElementById('gorilla-energy-progress');
+    const gorillaEnergyText = document.getElementById('gorilla-energy-text');
+    const gorillaDefenseProgress = document.getElementById('gorilla-defense-progress');
+    const gorillaDefenseText = document.getElementById('gorilla-defense-text');
+    const gorillaAttacksCountText = document.getElementById('gorilla-attacks-count');
+
+    const humansRemainingText = document.getElementById('humans-remaining-text');
+    const humansGridContainer = document.getElementById('humans-grid-container');
+
+    const attackBtn = document.getElementById('attack-btn');
+    const defendBtn = document.getElementById('defend-btn');
+    const resetBtn = document.getElementById('reset-btn');
+
+    const logEntriesContainer = document.getElementById('log-entries-container');
+
+    const gameOverSection = document.getElementById('game-over-section');
+    const gameOverMessage = document.getElementById('game-over-message');
+    const restartBtn = document.getElementById('restart-btn');
+
+    // Constantes do jogo
+    
+})
